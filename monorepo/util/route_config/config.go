047249eacf8083947d1ee2c5a37a0488d0ed320e
Matheus Soares <soares_kw@outlook.com>
2026-06-14 17:57:49 -0300

    fix: deploy

--- monorepo/util/route_config/config.go
+++ monorepo/util/route_config/config.go
@@ -16,25 +16,31 @@ var (
 )
 
 func Load() {
-
-	var err error
-	if err = godotenv.Load(); err != nil {
-		log.Fatal(err)
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file, reading config from environment variables")
 	}
 
 	user := os.Getenv("DB_USER")
 	password := os.Getenv("DB_PASSWORD")
 	dbName := os.Getenv("DB_NAME")
 
+	host := os.Getenv("DB_HOST")
+	if host == "" {
+		host = "localhost"
+	}
+	port := os.Getenv("DB_PORT")
+	if port == "" {
+		port = "3306"
+	}
+
+	var err error
 	Port, err = strconv.Atoi(os.Getenv("API_PORT"))
 	if err != nil {
 		Port = 9000
 	}
 
-	ConnectionString = fmt.Sprintf("%s:%s@tcp(localhost:3306)/%s?charset=utf8&parseTime=True&loc=Local",
-		user,
-		password,
-		dbName,
+	ConnectionString = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
+		user, password, host, port, dbName,
 	)
 
 	SecretKey = []byte(os.Getenv("SECRET_KEY"))
